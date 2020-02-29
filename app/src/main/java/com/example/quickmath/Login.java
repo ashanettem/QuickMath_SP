a256081988d74f42f8aaec77584731821141c532
SherBae <mitchellpineda15@gmail.com>
2020-02-29 18:09:19 -0500

    Fixed User.Class error removed import.

--- app/src/main/java/com/example/quickmath/Login.java
+++ app/src/main/java/com/example/quickmath/Login.java
@@ -1,21 +1,21 @@
 package com.example.quickmath;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.auth.User;
+
 
 public class Login extends AppCompatActivity {
 
@@ -52,27 +52,27 @@ public class Login extends AppCompatActivity {
                     for (QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots){
                         User currentUser = documentSnapshot.toObject(User.class);
 
-                        /*String email = currentUser.getEmail();
-                        //String password = currentUser.getPassword();
+                        String email = currentUser.getEmail();
+                        String password = currentUser.getPassword();
 
                         if (email.equals(user) && password.equals(pass)){
                             if (currentUser.getRole().equals("Student")){
-                                Intent intent = new Intent(view.getContext(),choices.class);
-                                intent.putExtra("User" , email);
-                                startActivity(intent);
+                                //Intent intent = new Intent(view.getContext(),choices.class);
+                                //intent.putExtra("User" , email);
+                                //startActivity(intent);
                                 Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
                             }
                             else if (currentUser.getRole().equals("Teacher")){
-                                Intent intent = new Intent(view.getContext(),adminDash.class);
-                                intent.putExtra("User" , email);
-                                startActivity(intent);
+                                //Intent intent = new Intent(view.getContext(),adminDash.class);
+                                //intent.putExtra("User" , email);
+                                //startActivity(intent);
                                 Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
                             }
 
                         }
                         else{
                             Toast.makeText(Login.this, "Please Try Again", Toast.LENGTH_LONG).show();
-                        }*/
+                        }
 
 
                         Intent i = new Intent(view.getContext(),MainActivity.class);
--- app/src/main/java/com/example/quickmath/User.java
+++ app/src/main/java/com/example/quickmath/User.java
@@ -1,4 +1,3 @@
-
 package com.example.quickmath;
 
 import com.google.firebase.firestore.Exclude;
